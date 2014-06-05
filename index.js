1b1c2df7261cf638e2e6e43f2834775719082e4e
tjmehta <tj@init.me>
2014-06-05 13:21:34 -0700

    typos errwhere.. :-/

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ var redisTypes = module.exports = {};
 
 fs.readdirSync(path.join(__dirname, 'lib'))
   .filter(jsFiles)
+  .map(basename)
   .forEach(function (filename) {
     redisTypes[filename] = require('.'+path.join('/', 'lib', filename));
   });
 
 function jsFiles (filename) {
   return jsFile.test(filename);
+}
+
+function basename (filename) {
+  return path.basename(filename, '.js');
 }
